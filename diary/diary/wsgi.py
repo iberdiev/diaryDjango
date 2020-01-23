848e0c30130f44229e3eabe926c16658ad8aa462
iberdiev <iberdiev1998@gmail.com>
2020-01-23 17:20:43 +0600

    deploy

--- diary/diary/wsgi.py
+++ diary/diary/wsgi.py
@@ -35,5 +35,5 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diary.settings")
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
